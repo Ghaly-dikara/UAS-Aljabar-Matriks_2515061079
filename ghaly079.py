c2afc088aafb91fb0252988311ab51d66eb35169
Ghaly-dikara <ghalyadkr@gmail.com>
2026-06-13 21:38:33 +0700

    Mengubah modul dan test code

---
+++ ghaly079.py
@@ -0,0 +1,33 @@
+def penjumlahan_vektor(a, b):
+    result = [a + b for a, b in zip(a, b)]
+    return result
+
+def pengurangan_vektor(a, b):
+    result = [a - b for a, b in zip(a, b)]
+    return result
+
+def transpose_vektor:
+
+def l2_norm:
+
+def l1_norm:
+
+def squared_l2_norm:
+
+def max_norm:
+
+def unit_vektor:
+
+def dot_product:
+
+def penjumlahan_matriks:
+
+def pengurangan_matriks:
+
+def perkalian_matriks:
+
+def transpose_matriks:
+
+def frobenius_norm:
+
+def matriks_inverse:
